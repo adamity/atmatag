410d9dc1a2035eb7367ea6362db9bb0ca83dc93e
Adamity <50904284+adamity@users.noreply.github.com>
2023-02-22 23:10:50 +0800

    try tp fix 419 error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" type="image/png" href="{{ asset('images/atmatag_2.ico') }}">
 
     <title>AtmaTag - Gets Lost Items Back to You</title>
