1d02e2c3cde0288f3be830b44eb033a4427923cd
Adamity <50904284+adamity@users.noreply.github.com>
2023-02-22 23:06:07 +0800

    try to fix blocked loading mixed active content

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <link rel="icon" type="image/png" href="{{ asset('images/atmatag_2.ico') }}">
 
     <title>AtmaTag - Gets Lost Items Back to You</title>
